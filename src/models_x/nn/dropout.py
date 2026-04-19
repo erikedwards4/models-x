a910f3fed9737242c0a9766a44644474cf960460
Erik Edwards <erik.edwards4@gmail.com>
2026-04-19 02:12:37 -0700

    nn/dropout update

--- src/models_x/nn/dropout.py
+++ src/models_x/nn/dropout.py
@@ -1,8 +1,11 @@
 """
 JAX functional equivalent of torch.nn.Dropout.
+
+Applies usual (element-wise) Dropout if training.
+Masks and scales to maintain the same expected value.
 """
 
-import jax
+from jax.random import bernoulli
 from jaxtyping import Float, Array
 
 __all__ = ["dropout"]
@@ -15,8 +18,13 @@ def dropout(batch: Float[Array, "..."],
             deterministic: bool = True,
             ) -> Float[Array, "..."]:
     """
-    Applies usual (element-wise) Dropout if training.
-    Masks and scales to maintain the same expected value.
+    p: dropout probability in [0.0, 1.0)
+       0.0 --> no dropout
+    key: JAX PRNG key
+         None --> no dropout
+    deterministic: similar to eval vs. train mode
+                   True  --> no dropout (eval mode)
+                   False --> use dropout (train mode)
     """
     # Only if training and valid
     if not deterministic and isinstance(key, Array) and 0.0 < p < 0.999999:
@@ -24,9 +32,9 @@ def dropout(batch: Float[Array, "..."],
         p_keep = 1.0 - p
 
         # Bernoulli mask
-        mask = jax.random.bernoulli(key=key,
-                                    p=p_keep,
-                                    shape=batch.shape)
+        mask = bernoulli(key=key,
+                         p=p_keep,
+                         shape=batch.shape)
 
         # Apply mask and scale
         batch = (batch * mask) / p_keep
