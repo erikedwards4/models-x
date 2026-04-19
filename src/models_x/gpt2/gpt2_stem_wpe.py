08a5f9798aa917b190a6819c4e20c850ab56b95d
Erik Edwards <erik.edwards4@gmail.com>
2026-04-18 23:20:07 -0700

    GPT-2 Stem WPE

--- src/models_x/gpt2/gpt2_stem_wpe.py
+++ src/models_x/gpt2/gpt2_stem_wpe.py
@@ -1,7 +1,7 @@
 """
-GPT-2 WTE (Word Token Embedding) part of the stem.
+GPT-2 WPE (Word Position Embedding) part of the stem.
 
-This is a look-up from integer token IDs in [0, vocab_size)
+This is a look-up from integer position IDs in [0, n_positions)
 to float embedding vectors with length d_model.
 """
 
@@ -13,32 +13,18 @@ import jax.numpy as jnp
 from jaxtyping import Float, Int, Array
 from models_x.gpt2.gpt2_config import GPT2Config
 
-__all__ = ["GPT2StemWTE"]
+__all__ = ["GPT2StemWPE"]
 
 
 @register_dataclass
 @dataclass
-class GPT2StemWTE():
+class GPT2StemWPE():
     """
-    GPT2 WTE (Word Token Embedding).
+    GPT2 WPE (Word Position Embedding).
     """
     # Replaces __init__ since registered dataclass
     metadata = dict(static=True)    # pylint: disable=use-dict-literal
     cfg: GPT2Config = field(metadata=metadata)
-
-    # def __init__(self: Self,
-    #              config: GPT2Config,
-    #              ) -> None:
-    #     """
-    #     vocab_size: size of vocab --> num_embeddings learned
-    #                 inputs to forward are ints in [0, vocab_size)
-    #     d_model: model dimension --> embedding_dim for learned vecs
-    #     dtype: JAX dtype
-    #     """
-    #     # Self attributes
-    #     self.vocab_size = int(config.vocab_size)
-    #     self.d_model = int(config.d_model)
-    #     self.dtype: jax.typing.DTypeLike = config.dtype
 
     def init_params(self: Self,
                     prng_key: Array,
@@ -47,30 +33,30 @@ class GPT2StemWTE():
         Initialize the parameters dict.
         """
         # Embedding weights shape
-        shape = (int(self.cfg.vocab_size),
+        shape = (int(self.cfg.n_positions),
                  int(self.cfg.d_model))
 
         # GPT-2 uses a normal dist with std = 0.02
         std = float(self.cfg.init_std)
 
         # Embedding weights
-        wte = jax.random.normal(key=prng_key,
+        wpe = jax.random.normal(key=prng_key,
                                 shape=shape,
                                 dtype=self.cfg.dtype,
-                                ) * std             # V x D
+                                ) * std             # P x D
 
-        return {'wte': wte}
+        return {'wpe': wpe}
 
     def __call__(self: Self,
                  params: dict[str, Array],
-                 input_ids: Int[Array, "B T"],      # noqa: F722
+                 position_ids: Int[Array, "B T"],   # noqa: F722
                  ) -> Float[Array, "B T D"]:        # noqa: F722
         """
-        B = batch_size (or micro-batch size)
+        B = batch_size (often 1 for WPE to broadcast)
         T = ntoks (num tokens, input seq len)
         D = d_model (num embeddings, model dim)
         """
         # torch.nn.Embedding lookup --> jnp.take
-        return jnp.take(a=params['wte'],
-                        indices=input_ids,
+        return jnp.take(a=params['wpe'],
+                        indices=position_ids,
                         axis=0)                     # B x T x D
--- tests/gpt2/test_gpt2_stem_wpe.py
+++ tests/gpt2/test_gpt2_stem_wpe.py
@@ -1,5 +1,5 @@
 """
-Pytest function for gpt2/gpt2_stem_wte.py.
+Pytest function for gpt2/gpt2_stem_wpe.py.
 """
 import time
 import pytest
@@ -7,41 +7,41 @@ import jax
 import jax.numpy as jnp
 from jaxtyping import Float, Array
 from models_x.gpt2.gpt2_config import GPT2Config
-from models_x.gpt2.gpt2_stem_wte import GPT2StemWTE
+from models_x.gpt2.gpt2_stem_wpe import GPT2StemWPE
 
 
-# gpt2_stem_wte.GPT2StemWTE
-@pytest.mark.parametrize("vocab_size", (50257, ))
+# gpt2_stem_wpe.GPT2StemWPE
+@pytest.mark.parametrize("n_positions", (1024, ))
 @pytest.mark.parametrize("d_model", (768, ))
 @pytest.mark.parametrize("dtype", (jnp.float32, ))
-def test_gpt2_stem_wte(vocab_size, d_model, dtype):
+def test_gpt2_stem_wpe(n_positions, d_model, dtype):
     """
-    Pytest gpt2_stem_wte.GPT2StemWTE.
+    Pytest gpt2_stem_wpe.GPT2StemWPE.
     """
     print("")
     device = jax.devices("gpu")[0]
     print_memory_stats(label="start")
 
     # Get config
-    config = GPT2Config(vocab_size=vocab_size,
+    config = GPT2Config(n_positions=n_positions,
                         d_model=d_model,
                         dtype=dtype)
 
     # Get mdl
-    wte = GPT2StemWTE(cfg=config)
-    assert isinstance(wte, GPT2StemWTE)
-    assert callable(wte)
-    assert wte.cfg.dtype == config.dtype == dtype
+    wpe = GPT2StemWPE(cfg=config)
+    assert isinstance(wpe, GPT2StemWPE)
+    assert callable(wpe)
+    assert wpe.cfg.dtype == config.dtype == dtype
 
     # Get PRNG key
     prng_key = jax.random.PRNGKey(seed=0)
 
     # Get params dict
-    params = wte.init_params(prng_key=prng_key)
-    assert 'wte' in params
-    assert isinstance(params['wte'], Array)
-    assert params['wte'].dtype == jnp.dtype(config.dtype)
-    assert 0.0 < jnp.std(params['wte']).item() < 1.0
+    params = wpe.init_params(prng_key=prng_key)
+    assert 'wpe' in params
+    assert isinstance(params['wpe'], Array)
+    assert params['wpe'].dtype == jnp.dtype(config.dtype)
+    assert 0.0 < jnp.std(params['wpe']).item() < 1.0
 
     # Check device (should default to GPU if using jaxlib)
     params = jax.device_put(x=params, device=device)
@@ -49,44 +49,43 @@ def test_gpt2_stem_wte(vocab_size, d_model, dtype):
     print(f"params.devices() = {sample_leaf.devices()}")
 
     # Make input data
-    nbatch = 4          # micro-batch size
+    nbatch = 1          # micro-batch size (broadcasted for WPE)
     ntoks = 16
-    size_in = (nbatch, ntoks)
-    input_ids = jax.random.randint(key=prng_key,
-                                   shape=size_in,
-                                   minval=0,
-                                   maxval=vocab_size,
-                                   dtype=jnp.int32,
-                                   ).to_device(device)
-    print(f"input_ids.device = {input_ids.device}")
+    position_ids = jnp.arange(start=0,
+                              stop=ntoks,
+                              step=1,
+                              dtype=jnp.int32,
+                              device=device,
+                              ).reshape(nbatch, ntoks)
+    print(f"position_ids.device = {position_ids.device}")
 
     # Test __call__
-    batch_out = wte(params=params,
-                    input_ids=input_ids)
+    batch_out = wpe(params=params,
+                    position_ids=position_ids)
     print(f"batch_out.dtype = {batch_out.dtype}")
     print(f"batch_out.shape = {batch_out.shape}")
     assert isinstance(batch_out, Float[jnp.ndarray, "..."])
     assert batch_out.dtype == jnp.dtype(config.dtype)
-    assert batch_out.device == input_ids.device
+    assert batch_out.device == position_ids.device
     assert batch_out.shape == (nbatch, ntoks, config.d_model)
     assert jnp.all(jnp.isfinite(batch_out))
 
     # Test embedding look-up
-    embd0 = params['wte'][input_ids[0, 0]]
+    embd0 = params['wpe'][position_ids[0, 0]]
     assert jnp.allclose(batch_out[0, 0], embd0)
 
     # Profile
 
     # Warmup (first jit call triggers compilation)
     for _ in range(4):
-        batch_out = wte(params, input_ids)
+        batch_out = wpe(params, position_ids)
         batch_out.block_until_ready()
 
     # Actual profiled runs
     times = []
     for _ in range(8):
         t0 = time.perf_counter()
-        batch_out = wte(params, input_ids)
+        batch_out = wpe(params, position_ids)
         batch_out.block_until_ready()
         times.append((time.perf_counter() - t0)*1000)
     times = jnp.array(times)
